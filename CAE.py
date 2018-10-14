1d9a0166ddcc8eb7d0091dfc5ff524e5c5700139
pkonowrocki <piotrek.konowrocki@gmail.com>
2018-10-14 21:02:27 +0200

    .

--- CAE.py
+++ CAE.py
@@ -1,7 +1,10 @@
 import torch.nn as nn
 import torch.optim as optim
+import torch
 from torch.autograd import Variable
-
+import torch.nn.functional as F
+import numpy as np
+import pickle
 class CAE(nn.Module):
     def __init__(self):
         super(CAE,self).__init__()
@@ -45,3 +48,39 @@ class CAE(nn.Module):
         h = self.encoder(x)
         d = self.decoder(h)
         return h, d
+    
+
+    
+    
+cae = CAE()
+
+pickle.dump(cae,open('cae_sgd.p','wb'))
+criterion = nn.L1Loss(True,True)
+optimizer = optim.Adagrad(cae.parameters(),0.1)
+
+def weight_loss(moduleA):
+    loss=0
+#    for parA in [moduleA.first.parameters(), moduleA.firstP.parameters(), moduleA.second.parameters(), moduleA.secondP.parameters(), moduleA.third.parameters(), moduleA.thirdP.parameters()]:
+#        for i in parA:
+#            loss+=torch.sum(torch.pow(i,2))
+    for i in moduleA.parameters():
+        loss+=torch.sum(torch.pow(i,2))
+    loss=loss*0.001
+    return loss
+
+for epoch in range(3):
+    running_loss = 0
+    for i in range(30000):
+        inputs = torch.from_numpy(np.random.randint(0,40,(1,2800))).float()
+        optimizer.zero_grad()
+        outputs = cae(inputs)
+        loss = criterion(outputs[1],inputs) + weight_loss(cae)
+        loss.backward()
+        optimizer.step()
+        running_loss+=loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+print('Finished')
+pickle.dump(cae,open('cae_sgd.p','wb'))
