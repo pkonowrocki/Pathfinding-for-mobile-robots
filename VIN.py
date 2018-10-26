169e4b6ac2649ae9e9a08cc97e27309b2843b69a
piotrek.konowrocki <piotrek.konowrocki@gmail.com>
2018-10-26 15:29:59 +0200

    .

--- VIN.py
+++ VIN.py
@@ -3,7 +3,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
-
+import numpy as np
+import pickle
+import time
+class Record():
+    def __init__(self):
+        self.S1=None
+        self.S2=None
+        self.OUTPUT=None
+        self.INPUT=None
 
 class VIN(nn.Module):
     def __init__(self):
@@ -37,22 +45,32 @@ class VIN(nn.Module):
         
         v = Variable(torch.zeros(r.size()))
         
-        for _ in range(20):
+        for _ in range(50):
             rv = torch.cat((r,v),dim=1)
-            
             q = self.conv_q(rv)
-            
             v, _ = torch.max(q,1,True)
-            
-        print(q.size())
-         
+#        print(q.size())
         q_out = torch.t(q[:,:,S1,S2][0])
-        print(q_out.size())
-        print(q_out)
+#        print(q_out.size())
+#        print(q_out)
         res = self.output(q_out)
         return res
         
 net = VIN()
-X = torch.ones([5,2,20,20])
-Y = net(X, torch.tensor([1,2,1,1,1]),torch.tensor([5,0,1,2,3]))
-print(Y)
+criterion = nn.L1Loss(True,True)
+optimizer = optim.Adagrad(net.parameters(),0.1)
+running_loss=0.0
+time0 = time.time()
+last_time = time0
+for epoch in range(1000):
+    for i in range(3500):
+        r = pickle.load(open('data/map'+str(i)+'.p','rb'))
+        optimizer.zero_grad()
+        outputs = net(torch.from_numpy(r.INPUT).float(), torch.from_numpy(r.S1).long(), torch.from_numpy(r.S2).long() )
+        loss = criterion(outputs,torch.from_numpy(r.OUTPUT).float())
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()/len(r.S2)
+        if(i%50==49):
+            print('Epoch: ['+str(epoch+1)+'/1000] Paths: ['+str(i+1)+'/3500], Loss: '+str(running_loss/(i+1))+', Time elapsed: '+str(np.round(time.time()-last_time))+'[s], Time left: '+str( np.round((time.time()-time0)*(1000*3500/(epoch+1)/(i+1))) ))
+    running_loss=0.0        
--- mapgenerator.py
+++ mapgenerator.py
@@ -4,6 +4,9 @@ import cv2 as cv
 import AStar
 import ThetaStar
 import rrtStar
+import time
+import pickle
+
 AStar = AStar.AStar()
 TStar = ThetaStar.ThetaStar()
 RStar = rrtStar.RRTstar()
@@ -32,14 +35,15 @@ def GenerateThetaStar():
     for i in range(len(S2)-1):
         cv.line(temp,S2[i+1][::1],S2[i][::1],120,1)
     
-    S2=list()
+    S2=np.expand_dims(np.array([start[0],start[1]]),0)
+    
     prev = start
     curr = start
     temp = np.transpose(temp)
     for i in Neighbours(curr):
         if(InBounds(i)):
             if(temp[i]>0):
-                S2.append(curr)
+                S2 = np.append(S2,np.expand_dims(np.array([curr[0],curr[1]]),0),axis=0)
                 prev=curr
                 curr=i
                 break
@@ -47,59 +51,83 @@ def GenerateThetaStar():
         for i in Neighbours(curr):
             if(InBounds(i) and i!=prev):
                 if(temp[i]>0):
-                    S2.append(curr)
+                    S2 = np.append(S2,np.expand_dims(np.array([curr[0],curr[1]]),0),axis=0)
                     prev=curr
                     curr=i
                     break
-    return S2
+    S2 = np.append(S2,np.expand_dims(np.array([end[0],end[1]]),0),axis=0)
+    return S2[1:,:]
 
-def GenerateRRTStar():
-    RStar.image = n[0,:,:]         
-    RStar.startend(start,end)
-    RStar.rrtStar(1)
-    S3 = RStar.shorten()
-    temp = np.zeros([40,40])
-    for i in range(len(S3)-1):
-        cv.line(temp,S3[i+1][::1],S3[i][::1],120,1)
-    prev = start
-    curr = start
-    #temp = np.transpose(temp)
-    S3=list()
-    for i in Neighbours(curr):
-        if(InBounds(i)):
-            if(temp[i]>0):
-                S3.append(curr)
-                prev=curr
-                curr=i
-                break
-    while(curr!=end):
-        for i in Neighbours(curr):
-            if(InBounds(i) and i!=prev):
-                if(temp[i]>0):
-                    S3.append(curr)
-                    prev=curr
-                    curr=i
-                    break
-    return S3
+def GenerateVector(S):
+    switch = {
+            (1,0):[1,0,0,0,0,0,0,0],
+            (1,1):[0,1,0,0,0,0,0,0],
+            (0,1):[0,0,1,0,0,0,0,0],
+            (-1,1):[0,0,0,1,0,0,0,0],
+            (-1,0):[0,0,0,0,1,0,0,0],
+            (-1,-1):[0,0,0,0,0,1,0,0],
+            (0,-1):[0,0,0,0,0,0,1,0],
+            (1,-1):[0,0,0,0,0,0,0,1]
+            }
+    res =np.expand_dims(np.array([0,0,0,0,0,0,0,0]),0)
+    for i in range(len(S)-1):
+        temp = tuple(S[(i+1),:]-S[i,:])
+        res = np.append(res, np.expand_dims(np.array(switch[temp]),0), axis=0)
+    return res[1:,:]
 
-for x in range(500):
-    print("["+str(x)+"]")
-    n = np.zeros([2,40,40])
-    for i in range(np.random.randint(200,400)):
-        temp = (0,np.random.randint(0,40),np.random.randint(0,40))
-        n[temp]=1
+def GenerateMap(x):
+    tempmap = np.zeros([10,10])
+    for i in range(int((65-15)*((x+1)/500)+15)):
+        temp = (np.random.randint(0,10), np.random.randint(0,10))
+        tempmap[temp]=1
+    tempmap = np.expand_dims(np.round(cv.resize(tempmap,(40,40))),0)
+    plot.imshow(tempmap[0,:,:])
+    n = np.concatenate((tempmap, np.zeros([1,40,40])))
+    return n
+
+def GenerateStartEnd():
     start = (np.random.randint(0,40),np.random.randint(0,40))
     end = (np.random.randint(0,40),np.random.randint(0,40))
     while(start==end or n[(0,start[0],start[1])]==1 or n[(0,end[0],end[1])]==1 or NotFeasible(start,end)):
         start = (np.random.randint(0,40),np.random.randint(0,40))
         end = (np.random.randint(0,40),np.random.randint(0,40))
-    n[(1,end[0],end[1])] = 1
-    print("START END DONE")
-    S1 = GenerateAStar()
-    print("A* DONE")
-    S2 = GenerateThetaStar()
-    print("THETA* DONE")
-    S3 = GenerateRRTStar()
-    print("RRT* DONE")
-    S4 = GenerateRRTStar()
+    return start, end
+
+class Record():
+    def __init__(self):
+        self.S1=None
+        self.S2=None
+        self.OUTPUT=None
+        self.INPUT=None
+
+
+zero_time = time.time()
+x=0
+iter=0
+for x in range(500):
+    start_time = time.time()
+    print("["+str(x)+"]")
     
+    n = GenerateMap(x)    
+    print("MAP GENERATED")
+    
+    for i in range(7):
+        print("["+str(x)+"]"+"["+str(i)+"]")
+        start, end = GenerateStartEnd()
+        n[(1,end[0],end[1])] = 1
+        print("START END DONE")
+        
+        S = GenerateThetaStar()
+        print("THETA* DONE")
+        record = Record()
+        record.OUTPUT = GenerateVector(S)
+        record.S1 = S[:-1,0]
+        record.S2 = S[:-1,1]
+        record.INPUT = np.expand_dims(n,axis=0)
+        for _ in range(len(record.S1)-1):
+            record.INPUT =  np.append(record.INPUT, np.expand_dims(n,axis=0), axis=0)
+        pickle.dump(record, open("data/map"+str(iter)+".p", "wb"))
+        iter=iter+1
+    print("Time: "+str(time.time()-start_time))
+    remaining_time = (499-x)*(time.time()-zero_time)/(x+1)
+    print("Remaining time:" + str(remaining_time))
